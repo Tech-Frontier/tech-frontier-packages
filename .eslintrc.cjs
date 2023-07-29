5675f5cc3c3b339f7e5154ee8876765f7dd2d39d
dp <126376945+divopsor@users.noreply.github.com>
2023-07-29 23:15:47 +0900

    feat(packages): set initial configuration (#1)
    * feat: 초기 레포 세팅
    
    * fix: eslint config
    
    * fix
    
    * fix
    
    * fix: 불필요 부분 제거
    
    * fix: ignore scripts
    
    * yarn
    
    * fix: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+module.exports = {
+  settings: {
+    // NOTE: 아래 내용을 추가해야 ts 로 추가한 alias paths 설정을 eslint 에서 읽을 수 있음
+    "import/resolver": {
+      typescript: {
+        project: "*/tsconfig.json",
+      },
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["react", "@typescript-eslint", "import"],
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "airbnb-typescript",
+    "next/core-web-vitals",
+    // @see https://github.com/import-js/eslint-plugin-import/tree/main#installation
+    "plugin:import/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ['./tsconfig.eslint.json', './packages/*/tsconfig.json'],
+  },
+  rules: {
+    "import/no-unresolved": "error",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+
+    // Ensure consistent use of file extension within the import path
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    "@next/next/no-html-link-for-pages": "off",
+
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal", "type", ["parent", "sibling", "index"], "unknown"],
+        pathGroups: [],
+        pathGroupsExcludedImportTypes: ["@tanstack*"],
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+
+    "@typescript-eslint/no-use-before-define": "off",
+
+    "no-multiple-empty-lines": ["error", { max: 1, maxBOF: 0, maxEOF: 0 }],
+    "react/jsx-props-no-multi-spaces": "error",
+    "key-spacing": ["error", { beforeColon: false }],
+    "eol-last": ["error", "always"],
+    "no-trailing-spaces": "error",
+    "no-multi-spaces": "error",
+  },
+};
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# tech-frontier-packages
+# tech-frontier
+
+> This project was created by using template of https://github.com/toss/yarn-standard-workspace
+
+## Purpose
+
+- This repo aims for making tools or libraries of frontend devops.
+- This repo uses lerna for managing package version.
+- We welcome contribution from everyone in the community.
+
+## Author
+
+- dp [@divopsor](https://github.com/divopsor)
+
+## License
+
+MIT © tech-frontier, Inc. See [LICENSE](./LICENSE) for details.
