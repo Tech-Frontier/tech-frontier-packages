9bc3f2c0d0505a5a675732cb6445faef20555c04
divopsor <divopsor@gmail.com>
2023-07-29 22:50:54 +0900

    fix: 불필요 부분 제거

--- packages/hello/src/bin/cli-internal.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('ts-node').register();
-
-require('./cli.ts');
--- packages/hello/src/bin/cli.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-
-import { hello } from '../index';
-
-const [, , name] = process.argv;
-
-hello(name);
