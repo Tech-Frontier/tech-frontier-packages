74521f517fa6de16960de7e4b6c5f8875269019c
divopsor <divopsor@gmail.com>
2023-08-06 14:56:10 +0900

    revert 내용

--- packages/ui-desktop/src/Header/Header.tsx
+++ packages/ui-desktop/src/Header/Header.tsx
@@ -1,14 +1,11 @@
 import { HTMLAttributes, forwardRef } from 'react';
-import { css } from '../styled-system/css';
 
 type HeaderProps = HTMLAttributes<HTMLDivElement>;
 
 export const Header = forwardRef<HTMLElement, HeaderProps>((props, ref) => {
   const { children, ...restProps } = props;
   return (
-    <header ref={ref} {...restProps} className={css({
-      backgroundColor: 'black',
-    })}>{children}</header>
+    <header ref={ref} {...restProps}>{children}</header>
   );
 });
 
