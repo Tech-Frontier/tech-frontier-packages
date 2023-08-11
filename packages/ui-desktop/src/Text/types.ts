fee38c871938536330b33b2b6db58308f6bb0705
pongdang <pongpong0121@kakao.com>
2023-08-12 02:21:39 +0900

    refactor: rank로 타입명 수정

--- packages/ui-desktop/src/Text/types.ts
+++ packages/ui-desktop/src/Text/types.ts
@@ -1,2 +1,2 @@
-export type TypographySize = '1' | '2' | '3' | '4' | '5' | '6';
+export type TypographyRank = '1' | '2' | '3' | '4' | '5' | '6';
 export type FontWeight = '100' | '200' | '300' | 'normal' | '500' | '600' | 'bold' | '800' | '900';
