e8812503669af7dbc4f39e80f655a4f0935e8f33
pongdang <pongpong0121@kakao.com>
2023-08-23 23:12:16 +0900

    fix: Header 컴포넌트의 서버 컴포넌트 지원을 위해 useEffect 제거

--- packages/ui-desktop/src/Header/Header.tsx
+++ packages/ui-desktop/src/Header/Header.tsx
@@ -1,5 +1,5 @@
-import { HTMLAttributes, forwardRef, useEffect, useState } from 'react';
-import { css, cx } from '../styled-system/css';
+import { HTMLAttributes, forwardRef } from 'react';
+import { cx } from '../styled-system/css';
 import { HeaderLogo } from './compound';
 import { headerBaseStyle, headerMoldStyle } from './style';
 
@@ -7,26 +7,27 @@ export type HeaderProps = HTMLAttributes<HTMLDivElement>;
 
 export const HeaderImpl = forwardRef<HTMLElement, HeaderProps>((props, ref) => {
   const { children, ...restProps } = props;
-  const [isScroll, setIsScroll] = useState(false);
+  // const [isScroll, setIsScroll] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = document.documentElement.scrollTop;
-      if (scrollTop > 0) {
-        setIsScroll(true);
-      } else {
-        setIsScroll(false);
-      }
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const scrollTop = document.documentElement.scrollTop;
+  //     if (scrollTop > 0) {
+  //       setIsScroll(true);
+  //     } else {
+  //       setIsScroll(false);
+  //     }
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, []);
 
   return (
     <>
       <header
         ref={ref}
-        className={cx(headerBaseStyle, isScroll && css({ backdropFilter: 'blur(10px)' }))}
+        // className={cx(headerBaseStyle, isScroll && css({ backdropFilter: 'blur(10px)' }))}
+        className={cx(headerBaseStyle)}
         {...restProps}
       >
         {children}
