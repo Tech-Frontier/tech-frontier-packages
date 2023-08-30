0c08445aa92b9afcbe89ba6bdb897d682ada7ead
pongdang <pongpong0121@kakao.com>
2023-08-31 00:44:51 +0900

    feat: blur prop 추가

--- packages/ui-desktop/src/Header/Header.tsx
+++ packages/ui-desktop/src/Header/Header.tsx
@@ -1,33 +1,20 @@
 import { HTMLAttributes, forwardRef } from 'react';
-import { cx } from '../styled-system/css';
+import { css, cx } from '../styled-system/css';
 import { HeaderLogo } from './compound';
 import { headerBaseStyle, headerMoldStyle } from './style';
 
-export type HeaderProps = HTMLAttributes<HTMLDivElement>;
+export interface HeaderProps extends HTMLAttributes<HTMLDivElement> {
+  blur?: boolean;
+}
 
 export const HeaderImpl = forwardRef<HTMLElement, HeaderProps>((props, ref) => {
-  const { children, ...restProps } = props;
-  // const [isScroll, setIsScroll] = useState(false);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const scrollTop = document.documentElement.scrollTop;
-  //     if (scrollTop > 0) {
-  //       setIsScroll(true);
-  //     } else {
-  //       setIsScroll(false);
-  //     }
-  //   };
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => window.removeEventListener('scroll', handleScroll);
-  // }, []);
+  const { blur, children, ...restProps } = props;
 
   return (
     <>
       <header
         ref={ref}
-        // className={cx(headerBaseStyle, isScroll && css({ backdropFilter: 'blur(10px)' }))}
-        className={cx(headerBaseStyle)}
+        className={cx(headerBaseStyle, blur && css({ backdropFilter: 'blur(10px)' }))}
         {...restProps}
       >
         {children}
